a63424f7a87d297d6ad952ffe19a708d79edf90e
Pranjl7 <pranjalsgbhatt@gmail.com>
2026-02-05 22:01:28 +0530

    feat: add pricing component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
 import Questions from './components/Questions';
+import Pricing from './components/Pricing';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <main className='relative min-h-screen max-w-6xl border-x border-black/10 mx-auto font-inter'>
       <Navbar />
       <Hero />
+      <Pricing />
       <Questions />
       <Footer />
     </main>
